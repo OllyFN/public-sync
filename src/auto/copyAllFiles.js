40eefe8478781432db6b83b9d5b414b6b326f3c7
OllyFN <281608@student.nottinghamcollege.ac.uk>
2023-11-11 11:28:53 +0000

    Config override option

--- src/auto/copyAllFiles.js
+++ src/auto/copyAllFiles.js
@@ -1,54 +1,68 @@
-import fs from 'fs-extra';
-import path from 'path';
-import chalk from 'chalk';
+import fs from "fs-extra";
+import path from "path";
+import chalk from "chalk";
 
 export default (config) => {
   const copiedFiles = [];
-  const DEFAULT_EXCLUDE = config.excludeDirs.map(item => new RegExp(item[0]));
+  const DEFAULT_EXCLUDE = config.excludeDirs.map((item) => new RegExp(item[0]));
 
   let logFn;
   if (config.debug.showCopied && config.debug.showNotCopied) {
-    logFn = (src, shouldCopy) => console.log(`${shouldCopy ? 'Copied' : 'Not copied'}: ${src}`);
+    logFn = (src, shouldCopy) =>
+      console.log(`${shouldCopy ? "Copied" : "Not copied"}: ${src}`);
   } else if (config.debug.showCopied) {
-    logFn = (src, shouldCopy) => { if (shouldCopy) console.log(`Copied: ${src}`); };
+    logFn = (src, shouldCopy) => {
+      if (shouldCopy) console.log(`Copied: ${src}`);
+    };
   } else if (config.debug.showNotCopied) {
-    logFn = (src, shouldCopy) => { if (!shouldCopy) console.log(`Not copied: ${src}`); };
+    logFn = (src, shouldCopy) => {
+      if (!shouldCopy) console.log(`Not copied: ${src}`);
+    };
   } else {
     logFn = () => {};
   }
 
   // Remove the destination directory before copying
-  fs.removeSync(config.publicDir);
-  fs.mkdirSync(config.publicDir, {recursive: true});
-  
+  if (config.override) {
+    fs.removeSync(config.publicDir);
+    fs.mkdirSync(config.publicDir, { recursive: true });
+  }
+
   try {
-    console.log(chalk.yellow('Starting to copy files...'));
+    console.log(chalk.yellow("Starting to copy files..."));
     const publicDirName = path.basename(config.publicDir);
     const privateDirPath = path.dirname(config.privateDir);
-    console.log(privateDirPath)
-    fs.readdirSync(config.privateDir).forEach(dir => {
+    console.log(privateDirPath);
+    fs.readdirSync(config.privateDir).forEach((dir) => {
       const dirPath = path.join(config.privateDir, dir);
       // Skip if the current dir is the destination directory
       if (dir === publicDirName) {
         return;
       }
-      if (!DEFAULT_EXCLUDE.some(excludedDir => excludedDir.test(dirPath))) {
+      if (!DEFAULT_EXCLUDE.some((excludedDir) => excludedDir.test(dirPath))) {
         fs.copySync(dirPath, path.join(config.publicDir, dir), {
           filter: (src) => {
-            if (DEFAULT_EXCLUDE.some(excludedDir => excludedDir.test(src)) || src === config.publicDir) {
+            if (
+              DEFAULT_EXCLUDE.some((excludedDir) => excludedDir.test(src)) ||
+              src === config.publicDir
+            ) {
               logFn(src, false);
               return false;
             }
             logFn(src, true);
             copiedFiles.push(path.relative(config.privateDir, src));
             return true;
-          }
+          },
         });
       }
     });
-    console.log(chalk.green(`Copied files: ${copiedFiles.join(', ')}\nTo: ${config.publicDir}`));
+    console.log(
+      chalk.green(
+        `Copied files: ${copiedFiles.join(", ")}\nTo: ${config.publicDir}`
+      )
+    );
   } catch (error) {
-    console.error(chalk.red('Error copying files: ' + error));
+    console.error(chalk.red("Error copying files: " + error));
     throw error;
   }
-}
+};
--- src/config.js
+++ src/config.js
@@ -1,33 +1,39 @@
-import fs from 'fs';
-import path from 'path';
-import chalk from 'chalk';
-import excludeDirs from './utils/excludeDirs.js';
-
+import fs from "fs";
+import path from "path";
+import chalk from "chalk";
+import excludeDirs from "./utils/excludeDirs.js";
 
 const defaultConfig = {
   debug: {
     showCopied: false,
-    showNotCopied: false
+    showNotCopied: false,
   },
-  publicDir: './publicSync/',
-  privateDir: './',
+  publicDir: "./publicSync/",
+  privateDir: "./",
   excludeDirs: excludeDirs,
   addExcludes: [],
-  interactive: false
+  interactive: false,
+  override: true,
 };
 
 function loadConfig() {
   try {
-    const packageJson = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf8'));
+    const packageJson = JSON.parse(
+      fs.readFileSync(path.resolve(process.cwd(), "package.json"), "utf8")
+    );
     if (packageJson.publicSync) {
       return { ...defaultConfig, ...packageJson.publicSync };
     } else {
-      throw new Error('publicSync not found in package.json');
+      throw new Error("publicSync not found in package.json");
     }
   } catch {
-    console.log(chalk.gray('Using default config. (use publicSyncConfig in package.json to change config)') + '\n');
+    console.log(
+      chalk.gray(
+        "Using default config. (use publicSyncConfig in package.json to change config)"
+      ) + "\n"
+    );
     return defaultConfig;
   }
 }
 
-export default loadConfig
+export default loadConfig;
